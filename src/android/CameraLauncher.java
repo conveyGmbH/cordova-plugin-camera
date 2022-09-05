ee3265468cd90123280dd0a1cd967e7041b78ce5
Hung <quanghungbui1@gmail.com>
2022-09-05 16:42:38 +0200

    fix workaround securityexception (see commit 900ca06c - "workaround SecurityException -> cleanup function)

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -1361,7 +1361,12 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                 id--;
             }
             Uri uri = Uri.parse(contentStore + "/" + id);
+			// @hung 21.07.2022 workaround 
+		    try {
             this.cordova.getActivity().getContentResolver().delete(uri, null, null);
+            } catch (SecurityException e) {
+                LOG.d(LOG_TAG, "Swallowed error, probably caused by missing permissions, while trying to delete duplicate image.");
+            }
             cursor.close();
         }
     }
